5d65415ca9c8e46093b5a218a06bc1863442f84c
saposki <saposki@gmail.com>
2015-11-12 19:54:42 -0800

    updated script dependencies on index.html to cdn

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+// angular 
+// angular ui cdn 
+// add BONDFIRE to header 
+
+//[x] landing done 
+//[x] 
+//[] add denpendencies to all js files 
+
+
+angular.module('bootCamp', [])
+	.controller('camper', function(){
+		this.stuff = newStuff;
+	});
+
+	newStuff = ['orobosa', 'victor'];
