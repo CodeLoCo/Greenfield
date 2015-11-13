6f35300659d3860d58b27c9ec455b52e28510917
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-11-12 17:44:12 -0800

    added routing and controller templates for landing, login, signup, and userHome pages

---
+++ client/app/landingPage/landingPage.js
@@ -0,0 +1,10 @@
+// module for landingPage view
+angular.module('bootCamp.landingPage', [
+	// add any factory dependencies here
+
+])
+
+// if there are factory dependencies, add function it depends on as a function argument
+.controller('LandingPageController', function($scope) {
+	console.log("inside LandingPageController---------------");
+})
---
+++ client/app/login/login.js
@@ -0,0 +1,10 @@
+// module for login page view
+angular.module('bootCamp.login', [
+	// add any factory dependencies here
+
+])
+
+// if there are factory dependencies, add function it depends on as a function argument
+.controller('LoginController', function($scope) {
+	console.log("inside LoginController---------------");
+})
