806601bb835b6570f10bddf6ccb116f87fc91cfb
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-11-12 19:07:14 -0800

    added structure, views, and routing for blog and sandbox views

---
+++ client/app/blog/blog.js
@@ -0,0 +1,10 @@
+// module for blog view
+angular.module('bootCamp.blog', [
+	// add any factory dependencies here
+
+])
+
+// if there are factory dependencies, add function it depends on as a function argument
+.controller('BlogController', function($scope) {
+	console.log("inside BlogController---------------");
+})
