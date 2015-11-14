8c6d299547fbcde44b9f3aa6caf6a16d78fba223
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-11-13 17:29:17 -0800

    login form redirects to userHome page on enter or when submit button is clicked

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -5,6 +5,14 @@ angular.module('bootCamp.login', [
 ])
 
 // if there are factory dependencies, add function it depends on as a function argument
-.controller('LoginController', function($scope) {
+.controller('LoginController', function($scope, $location) {
 	console.log("inside LoginController---------------");
+
+	// this function runs when user tries to log in
+	// should authenticate then redirect to userHome page
+	$scope.logUserIn = function() {
+		$location.path('/userHome');
+
+	}
+
 })
