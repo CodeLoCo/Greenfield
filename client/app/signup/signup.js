86cdf29c97f617b50d2b71a7df4712b7ff067ff3
TGAStudent <tga.student@telegraphacademy.com>
2015-11-16 12:58:02 -0800

    implementing controller logic - signup.js

--- client/app/signup/signup.js
+++ client/app/signup/signup.js
@@ -1,18 +1,35 @@
 // module for signup page view
-angular.module('bootCamp.signup', [
-	// add any factory dependencies here
-
-])
+// var Firebase = require("firebase");
+angular.module('bootCamp.signup', ['firebase'])
 
 // if there are factory dependencies, add function it depends on as a function argument
-.controller('SignupController', function($scope, $location) {
-	console.log("inside SignupController---------------");
+
+.factory('Profile', ['$firebaseObject'], function($firebaseObject){
+  return function(username){
+    var ref = new Firebase("https://bondfire2.firebaseio.com");
+    var profileRef = ref.child(username);
+    return $firebaseObject(profileRef);
+  }
+
+})
+.controller('SignupController', ['$scope', 'Profile', '$location',function($scope, Profile) {
+  console.log("inside SignupController---------------");
+    console.log(ref);
 
 	// this function runs when user tries to log in
 	// should authenticate then redirect to userHome page
-	$scope.signUserUp = function() {
-		$location.path('/userHome');
+  	$scope.signUserUp = function() {
+  		$location.path('/userHome');
+      //$scope.data = $firebaseObject(ref);
+      $scope.profile = Profile("firstname")
+      $scope.profile.$save().then(function(){
+        alert('save this shit!!');
 
-	}
+      }).catch(function(error){
+        alert('Error!');
+      });
+      console.log($scope.data)
 
-})
+  	}
+  }
+]);
